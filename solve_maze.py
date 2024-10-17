1fdc309df1407ac62a4046ec49f57c3b5afc8a75
Mengjia Lyu <mengjialyu@Mengjias-MacBook-Pro.local>
2024-10-16 19:21:26 -0700

    fix error

--- solve_maze.py
+++ solve_maze.py
@@ -1,28 +1,27 @@
 def paths_through_maze(maze):
-    visited = set()
     result = 0
-    directions = [(1,0), (-1,0), (0,1), (0,-1)]
+    paths = []
+    directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
     num_of_rows, num_of_cols = len(maze), len(maze[0])
 
-    def find_paths(row, col):
+    def find_paths(row, col, current_path):
         nonlocal result
-        # if we reach bottom right, then we add 1 to result
+        nonlocal directions
         if row == num_of_rows - 1 and col == num_of_cols - 1:
             result += 1
-            return 
-        visited.add((row, col))
-        for dir in directions:
-            next_row, next_col = tuple(map(lambda i, j: i + j, (row, col), dir))
-            if is_valid(next_row, next_col):
-                find_paths(next_row, next_col)
+            paths.append(current_path + [(row, col)])
+            return
+                
+        for dr, dc in directions:
+            next_row, next_col = row + dr, col + dc
+            if is_valid(next_row, next_col, current_path):
+                find_paths(next_row, next_col, current_path + [(row, col)])
 
-        visited.remove((row, col))
-
-    def is_valid(row, col):
-        return row >= 0 and col >= 0 and row < num_of_rows and \
-        col < num_of_cols and not maze[row][col] and (row, col) not in visited
+    def is_valid(row, col, current_path):
+        return 0 <= row < num_of_rows and 0 <= col < num_of_cols and \
+        (maze[row][col] == 0) and (row, col) not in current_path
     
-    find_paths(0,0)
+    find_paths(0,0,[])
     return result
 
 if __name__ == "__main__":
